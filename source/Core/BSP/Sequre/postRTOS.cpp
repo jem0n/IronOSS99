6f5a88db012e99a10e45437d77dfa835e8471254
markusstadtmann.ext@levatis.com <markus.stadtmann.ext@levatis.com>
2026-08-26 09:21:15 +0200

    Beep only once per soldering session when the set point is first reached; drop the boot chirp

--- source/Core/BSP/Sequre/postRTOS.cpp
+++ source/Core/BSP/Sequre/postRTOS.cpp
@@ -1,12 +1,4 @@
 #include "BSP.h"
-#include "Pins.h"
 
 // Initialisation to be performed with scheduler active
-void postRToSInit() {
-#ifdef BUZZER_Pin
-  // Short chirp at boot (the stock firmware plays a start-up melody), also handy to verify the buzzer
-  setBuzzer(true);
-  delay_ms(80);
-  setBuzzer(false);
-#endif
-}
+void postRToSInit() {}
