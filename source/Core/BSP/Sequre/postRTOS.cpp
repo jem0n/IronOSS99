a5ef11dd6cebb8876ba67f69f84dabb2cc668cf1
jem0n <123236915+jem0n@users.noreply.github.com>
2026-08-30 05:08:46 +0800

    Update postRTOS.cpp

--- source/Core/BSP/Sequre/postRTOS.cpp
+++ source/Core/BSP/Sequre/postRTOS.cpp
@@ -1,12 +1,17 @@
 #include "BSP.h"
-#include "Pins.h"
+// #include "Pins.h"
 
 // Initialisation to be performed with scheduler active
-void postRToSInit() {
-#ifdef BUZZER_Pin
+// void postRToSInit() {
+// #ifdef BUZZER_Pin
   // Short chirp at boot (the stock firmware plays a start-up melody); silent when the buzzer setting is off
-  setBuzzer(true);
-  delay_ms(80);
-  setBuzzer(false);
-#endif
-}
+  // setBuzzer(true);
+  // delay_ms(80);
+  // setBuzzer(false);
+// #endif
+// }
+
+// Initialisation to be performed with scheduler active.
+// Note: this runs at the start of the power thread, right before the USB-PD negotiation is kicked off, so it must
+// not block - the start-up chirp lives in the GUI thread (after the boot logo) for that reason.
+void postRToSInit() {}
